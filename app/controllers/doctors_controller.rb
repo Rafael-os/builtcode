a4f9c33f38dc490b0aa2d7a0844dea13d7ec4761
rafael osawa <osawa.rafael@gmail.com>
2021-06-14 13:54:30 +0300

    added gems

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,22 +1,43 @@
 class DoctorsController < ApplicationController
   def index
+    @doctors = Doctor.all
   end
 
   def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to doctors_path
+    else
+      render "new"
+    end
   end
 
   def new
+    @doctor = Doctor.new
   end
 
   def edit
+    @doctor = Doctor.find(params[:id])
   end
 
   def show
+    @doctor = Doctor.find(params[:id])
   end
 
   def update
+    @doctor = Doctor.find(params[:id])
+    @doctor.update(params[:doctor])
   end
 
   def destroy
+    @doctor = Doctor.find(params[:id])
+    @doctor.destroy
+    redirect_to doctors_path
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :crm, :crm_uf)
   end
 end
