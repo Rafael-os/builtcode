ec1e158ce32c1aa5444c2a4f4b6bcd4713906f5f
rafael osawa <osawa.rafael@gmail.com>
2021-06-15 13:25:42 +0300

    added appointment crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :doctors, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :patients, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :appointments, only: [:index, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
