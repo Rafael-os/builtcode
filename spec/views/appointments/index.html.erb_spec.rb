c7a2c7e36a49d22818536c2f3142808e3850ca86
rafael osawa <osawa.rafael@gmail.com>
2021-06-15 19:00:30 +0300

    Added tests

--- spec/views/appointments/index.html.erb_spec.rb
+++ spec/views/appointments/index.html.erb_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "appointments/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "display all the appointments" do
+    doctor1 = Doctor.create!(name: 'Strange', crm: '1234', crm_uf: "SP")
+    patient1 = Patient.create!(name: 'Paciente', birth_date: '1990/01/01', cpf: "12345678901")
+    doctor2 = Doctor.create!(name: 'House', crm: '1234', crm_uf: "PR")
+    patient2 = Patient.create!(name: 'Paciente2', birth_date: '1990/01/05', cpf: "12345675342")
+    assign(:appointments, [
+      Appointment.create!(starts_at: DateTime.now + 10, ends_at: DateTime.now + 10.01, patient_id: patient1.id, doctor_id: doctor2.id),
+      Appointment.create!(starts_at: DateTime.now + 5, ends_at: DateTime.now + 5.01, patient_id: patient2.id, doctor_id: doctor2.id)
+    ])
+
+    render
+
+    expect(rendered).to match /1/
+    expect(rendered).to match /2/
+  end
 end
--- spec/views/appointments/new.html.erb_spec.rb
+++ spec/views/appointments/new.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "appointments/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders the new appointment form" do
+    doctor1 = Doctor.create(name: 'Strange', crm: '1234', crm_uf: "SP")
+    patient1 = Patient.create(name: 'Paciente', birth_date: '1990/01/01', cpf: "12345678901")
+
+    @appointment = assign(:appointment, [
+      Appointment.create!(starts_at: DateTime.now + 10, ends_at: DateTime.now + 10.01, patient_id: patient1.id, doctor_id:doctor1.id),
+    ])
+    render
+
+    assert_select "form[action=?][method=?]", appointment_path(@appointment), "post" do
+    
+        assert_select "input[type=?]", "submit"
+
+    end
+  end
 end
