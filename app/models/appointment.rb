aa51544a7b937b4ef131cca0c0253ba2d903b0d8
rafael osawa <osawa.rafael@gmail.com>
2021-06-15 16:06:27 +0300

    Added customs error messages

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,37 +3,43 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
 
   validates :starts_at, :ends_at, presence: true
-  validate :start_date_not_past?, :start_hour_less_than_9?, :start_hour_more_than_18?, :lunch_break?, :consultation_time
+  validate :start_date_not_past?, :start_hour_less_than_9?, :start_hour_more_than_18?, :lunch_break?, :consultation_time, :consultation_period
 
   private
 
   def start_date_not_past?
     if starts_at.present? && starts_at < DateTime.now
-      errors.add :starts_at, "must be after current date"
+      errors.add :starts_at, "Consulta nao pode ser marcado no passado"
     end
   end
 
   def start_hour_less_than_9?
     if starts_at.present? && starts_at.hour < 9
-      errors.add :starts_at, "must be after 9:00"
+      errors.add :starts_at, "Consulta tem que ser depois das 9:00"
     end
   end
       
   def start_hour_more_than_18?
     if starts_at.present? && starts_at.hour > 18
-      errors.add :starts_at, "must be before 18:00"
+      errors.add :starts_at, "Consulta tem que ser antes das 18:00"
     end
   end
 
   def lunch_break?
     if starts_at.present? && starts_at.hour >= 12 && starts_at.hour < 13
-      errors.add :starts_at, "cannot schedule during lunch break"
+      errors.add :starts_at, "Nao pode criar consulta no horario de almoco"
     end
   end
 
   def consultation_time
     if starts_at.present? && ends_at.present? && ( ends_at - starts_at ) > 1800
-      errors.add :ends_at, "consult cannot last more than 30 minutes"
+      errors.add :ends_at, "Consulta nao pode durar mais de 30 minutos"
+    end
+  end
+
+  def consultation_period
+    if starts_at.present? && ends_at.present? && ( ends_at - starts_at ) < 0
+      errors.add :ends_at, "Consulta nao pode terminar antes de comecar"
     end
   end
 end
