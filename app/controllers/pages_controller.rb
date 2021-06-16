ca9dda36297ed37ff3773e19b9fcadb8b15d95d4
rafael osawa <osawa.rafael@gmail.com>
2021-06-16 12:05:50 +0300

    Added consultas realizadas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,6 @@ class PagesController < ApplicationController
   def dashboard
     @doctors = Doctor.all
     @ordered = @doctors.order(created_at: :desc)
-    appointments_done
-  end
-
-  def appointments_done
-    @finished_appointments = Appointment.where('ends_at < ?', DateTime.now).count
+    @finished_appointments = Appointment.where('ends_at < ?', Time.now).count
   end
 end
