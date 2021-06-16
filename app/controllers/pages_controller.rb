87bda4afe611b8e156da04f41b6d7fb0933a7619
rafael osawa <osawa.rafael@gmail.com>
2021-06-16 14:10:42 +0300

    Added period filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,12 @@ class PagesController < ApplicationController
     @doctors = Doctor.all
     @ordered = @doctors.order(created_at: :desc)
     @finished_appointments = Appointment.where('ends_at < ?', Time.now).count
-    @patients_number = Appointment.where("starts_at between ? and ?", @start_date, @end_date)
-    @search = Search.new(params[:search])
-    @numbers = @search.scope
+    if params[:search]
+      a = params[:search]
+      @start = a["start_date"]
+      @end = a["end_date"]
+      @patients_number = Appointment.where("starts_at between ? and ?", Date.parse(@start).beginning_of_day, Date.parse(@end).end_of_day)
+      @patients_number = @patients_number.distinct.count('patient_id')
+    end
   end
 end
